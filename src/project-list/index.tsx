e6990eff0effd488ff25faca9f934fa9b0e7f888
LiangMengEn <mengen.liang@yo-ren.com>
2022-12-17 23:13:30 +0800

    feat: js 转为 ts

--- src/project-list/index.tsx
+++ src/project-list/index.tsx
@@ -6,18 +6,19 @@ import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
-
   const [param, setParam] = useState({ name: "", personId: "" });
 
   const [users, setUsers] = useState([]);
 
   const [list, setList] = useState([]);
 
-  const debouncedParam = useDebounce(param, 200)
+  const debouncedParam = useDebounce(param, 200);
 
   useEffect(() => {
     return () => {
-      fetch(`${apiUrl}/projects?${qs.stringify(clearObject(debouncedParam))}`).then(async res => {
+      fetch(
+        `${apiUrl}/projects?${qs.stringify(clearObject(debouncedParam))}`
+      ).then(async (res) => {
         if (res.ok) {
           setList(await res.json());
         }
@@ -26,17 +27,19 @@ export const ProjectListScreen = () => {
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async res => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
   });
 
-  return <div>
-    {/*搜索框*/}
-    <SearchPanel param={param} setParam={setParam} users={users} />
-    {/*用户表格*/}
-    <List list={list} users={users} />
-  </div>;
+  return (
+    <div>
+      {/*搜索框*/}
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      {/*用户表格*/}
+      <List list={list} users={users} />
+    </div>
+  );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 
-export const isFalse = (value) => (value === 0 ? false : !value);
-export const clearObject = (obj) => {
+export const isFalse = (value: number): boolean =>
+  value === 0 ? false : !value;
+export const clearObject = (obj: Object) => {
   const result = { ...obj };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalse(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay);
